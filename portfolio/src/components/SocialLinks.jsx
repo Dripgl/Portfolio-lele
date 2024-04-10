0d5d72e44a0e6503489024c1396f5530610796aa
Gabri <gabriele.zito@lc-sw.com>
2024-04-10 16:40:11 +0200

    fix social

--- portfolio/src/components/SocialLinks.jsx
+++ portfolio/src/components/SocialLinks.jsx
@@ -9,9 +9,8 @@ const SocialLinks = () => {
       <link href="//netdna.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet" />
       <nav className="social">
         <ul>
-          <li><a href="">Instagram <i className="fa fa-instagram"></i></a></li>
-          <li><a href="https://github.com/Dripgl">Github <i className="fa fa-github"></i></a></li>
           <li><a href="https://www.linkedin.com/in/gabriele-zito-987452217/">Linkedin <i className="fa fa-linkedin"></i></a></li>
+          <li><a href="https://github.com/Dripgl">Github <i className="fa fa-github icon_social"></i></a></li>
         </ul>
       </nav>
     </>
